3da842d7ecee67b9fef1c9417176274cc929047e
Ramadhana Pradipta Adi <95260089+890Pradipta@users.noreply.github.com>
2021-11-30 15:33:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(
+"Nama saya adalah "
++ nama +
+", asal sekolah/universitas "
++ asalSekolah +
+" jurusan "
++ jurusan +
+". Alasan saya tertarik dengan programming adalah "
++ alasanSukaProgramming)
